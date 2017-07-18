0f62da65a970dd2eb83e93295bd195c1ff7bdf76
ryhanlon <ryhanlon@gmail.com>
2017-07-17 23:04:12 -0700

    Set up the files, migrated, added settings, made the Post model.

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uo2evkb_ed)nqcm4tec70d*%txwnf7=)37-h(0jd!l(2kip2+8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'ryhanlon.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.apps.MysiteConfig',
+    # 'djangoblog.apps.MysiteConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'djangoblog.wsgi.application'
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
