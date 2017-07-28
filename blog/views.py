b203341dec93a89e7991b8efe7a74d15a9e22911
ryhanlon <ryhanlon@gmail.com>
2017-07-27 20:00:18 -0700

    Made new template (post_edit.html) with updated views.py, urls.py

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,8 @@ def post_list(request):
     Published blog posts sorted by published_date
     """
     posts = Post.objects.filter(published_date__lt=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    context = {'posts': posts}
+    return render(request, 'blog/post_list.html', context)
 
 
 def post_detail(request, pk):
@@ -18,7 +19,8 @@ def post_detail(request, pk):
     Publish the details of the post on a separate link.
     """
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
 
 
 def post_new(request):
@@ -26,4 +28,5 @@ def post_new(request):
     Publish the interface to add a new blog post.
     """
     form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context)
